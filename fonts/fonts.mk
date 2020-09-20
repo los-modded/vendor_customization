2706a89ef9f5f8ed6e05c8d20408107f493b6dd3
Danny Lin <danny@kdrag0n.dev>
2020-09-20 16:59:54 -0700

    Add custom fonts for enhanced typography
    This adds the Fira Code, Inter, and Source Serif Pro fonts to replace
    Cutive Mono/Droid Sans Mono, Roboto, and Noto Serif respectively. All of
    their respective copyright notices and licenses have been added.
    
    These fonts have been patched to use the same names as the fonts they
    are replacing for maximum compatibility, as some apps (e.g. Firefox
    Fenix) look for fonts by their TTF/OTF name rather than using Android's
    fonts.xml definitions.
    
    All of these fonts are licensed under the Open Font License (OFL).
    
    [los-modded: Removed unwanted fonts and added copyright]
    
    Change-Id: I3265b0f30b379a1350e47bbdb914e124c5227585

---
+++ fonts/fonts.mk
@@ -0,0 +1,17 @@
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES += \
+    Roboto-BlackItalic.ttf \
+    Roboto-Black.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Bold.ttf \
+    Roboto-Italic.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Light.ttf \
+    Roboto-MediumItalic.ttf \
+    Roboto-Medium.ttf \
+    Roboto-Regular.ttf \
+    Roboto-ThinItalic.ttf \
+    Roboto-Thin.ttf
